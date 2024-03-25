8f759334c29af1412a1ffc8536d510b93693e4de
SNGSupergirl <123931152+SNGSupergirl@users.noreply.github.com>
2024-03-24 18:05:10 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -159,7 +159,7 @@ client.on('interactionCreate', async (interaction) => {
         logEmbed.thumbnail = { url: thumbnailUrl };
 
         // Edit the reply to show the selected thumbnail
-        await message.update({ embeds: [logEmbed], components: [] });
+        await interaction.update({ embeds: [logEmbed], components: [] });
     }
 });
 
